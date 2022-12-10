bb6db3d4c6222e875114c3b77e1c8861f57b61dc
DangNguyen146 <19521317@gm.uit.edu.vn>
2022-12-10 23:11:21 +0700

    add new daugia

---
+++ daugia/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DaugiaConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'daugia'
--- webdaugia/settings.py
+++ webdaugia/settings.py
@@ -11,9 +11,11 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -37,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'daugia',
 ]
 
 MIDDLEWARE = [
@@ -120,6 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
