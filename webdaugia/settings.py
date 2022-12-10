3e7562e8f978025140a86d6e612f6e26e3a7ef84
DangNguyen146 <19521317@gm.uit.edu.vn>
2022-12-10 23:04:10 +0700

    add setting

--- webdaugia/settings.py
+++ webdaugia/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'webdaugia.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'webdaugia',
+        'USER': 'webdaugia',
+        'PASSWORD': '123456',
+        'HOST': '192.168.206.130',
+        'PORT': '5432',
     }
 }
 
